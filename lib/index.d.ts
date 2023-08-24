fc858fd3be510b821a7889f0930d6f35031e0e48
moayad_khader <moayadkhader99@gmail.com>
2023-08-24 15:54:22 +0300

    Setup Package

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,8 @@
-export declare const extractDate: (text: string) => import("./interface/search.interface").DateTimeSearchResult;
+import constants from "./constants";
+import Desclude from "./data/Desclude";
+import { TokenizedQT } from "./interface/search.interface";
+import { getDateTimeTags } from "./utils/search.datetime.utils";
+declare const descludeNotNeeded: (tqt: TokenizedQT) => TokenizedQT;
+declare const toEnglishTokenize: (str: string) => TokenizedQT;
+declare const extractDate: (text: string) => import("./interface/search.interface").DateTimeSearchResult;
+export { constants, Desclude, TokenizedQT, getDateTimeTags, toEnglishTokenize, descludeNotNeeded, extractDate };
